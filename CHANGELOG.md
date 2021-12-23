3596ff45eae2d4b6106ab7f2bddc039fe87e7537
FlagElitov <61709509+FlagElitov@users.noreply.github.com>
2021-12-23 22:21:11 +0200

    added code

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,3 @@
-## 0.0.1
+## [1.0.0] - 2021-12-23
 
-* TODO: Describe initial release.
+* Created package
--- lib/color_print.dart
+++ lib/color_print.dart
@@ -1,7 +1,22 @@
 library color_print;
+import 'dart:developer' as developer;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
+// Blue text
+void logInfo(String msg) {
+  developer.log('\x1B[34m$msg\x1B[0m');
+}
+
+// Green text
+void logSuccess(String msg) {
+  developer.log('\x1B[32m$msg\x1B[0m');
+}
+
+// Yellow text
+void logWarning(String msg) {
+  developer.log('\x1B[33m$msg\x1B[0m');
+}
+
+// Red text
+void logError(String msg) {
+  developer.log('\x1B[31m$msg\x1B[0m');
 }
