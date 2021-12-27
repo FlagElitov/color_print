3255417162bfcf51a6e80223a5eaf65c34e4b5c1
FlagElitov <61709509+FlagElitov@users.noreply.github.com>
2021-12-27 21:32:55 +0200

    added name

--- lib/color_print.dart
+++ lib/color_print.dart
@@ -2,21 +2,21 @@ library color_print;
 import 'dart:developer' as developer;
 
 // Blue text
-void logInfo(String msg) {
-  developer.log('\x1B[34m$msg\x1B[0m');
+void logInfo(String msg, [String name = '']) {
+  developer.log('\x1B[34m$msg\x1B[0m', name: name);
 }
 
 // Green text
-void logSuccess(String msg) {
-  developer.log('\x1B[32m$msg\x1B[0m');
+void logSuccess(String msg, [String name = '']) {
+  developer.log('\x1B[32m$msg\x1B[0m', name: name);
 }
 
 // Yellow text
-void logWarning(String msg) {
-  developer.log('\x1B[33m$msg\x1B[0m');
+void logWarning(String msg, [String name = '']) {
+  developer.log('\x1B[33m$msg\x1B[0m', name: name);
 }
 
 // Red text
-void logError(String msg) {
-  developer.log('\x1B[31m$msg\x1B[0m');
+void logError(String msg, [String name = '']) {
+  developer.log('\x1B[31m$msg\x1B[0m', name: name);
 }
