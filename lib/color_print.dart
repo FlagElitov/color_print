c2c396dc91ce8bc5546a4beede598f749d2000b1
Artur <61709509+FlagElitov@users.noreply.github.com>
2022-03-10 18:55:40 +0200

    added dynamic type

--- lib/color_print.dart
+++ lib/color_print.dart
@@ -2,21 +2,21 @@ library color_print;
 import 'dart:developer' as developer;
 
 // Blue text
-void logInfo(String msg, [String name = '']) {
-  developer.log('\x1B[34m$msg\x1B[0m', name: name);
+void logInfo(dynamic msg, [String name = '']) {
+  developer.log('\x1B[34m${msg.toString()}\x1B[0m', name: name);
 }
 
 // Green text
-void logSuccess(String msg, [String name = '']) {
-  developer.log('\x1B[32m$msg\x1B[0m', name: name);
+void logSuccess(dynamic msg, [String name = '']) {
+  developer.log('\x1B[32m${msg.toString()}\x1B[0m', name: name);
 }
 
 // Yellow text
-void logWarning(String msg, [String name = '']) {
-  developer.log('\x1B[33m$msg\x1B[0m', name: name);
+void logWarning(dynamic msg, [String name = '']) {
+  developer.log('\x1B[33m${msg.toString()}\x1B[0m', name: name);
 }
 
 // Red text
-void logError(String msg, [String name = '']) {
-  developer.log('\x1B[31m$msg\x1B[0m', name: name);
+void logError(dynamic msg, [String name = '']) {
+  developer.log('\x1B[31m${msg.toString()}\x1B[0m', name: name);
 }
